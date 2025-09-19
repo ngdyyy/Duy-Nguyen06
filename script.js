e60eb20ce6f270592c0d22689b09ed3a3dc94e68
ngdyyy <nqduy130206@gmail.com>
2025-09-19 08:17:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = 2;
+let b = 3;
+
+console.log("Tổng= ", a+b);
+console.log("Hiệu= ", a-b);
+console.log("Tích= ",a*b);
+console.log("Thương= ",a/b);
+console.log("Số dư= ",a%b);
+console.log("Luỹ thừa= ",a**b);
